73e99266b8e9d313a3c5afa3c867aa11ed12d321
takpika <takpika000@gmail.com>
2021-01-23 19:09:45 +0800

    Initial Commit

---
+++ Spotify-Karaoke/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Spotify-Karaoke
+//
+//  Created by takumi saito on 2021/01/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Spotify-Karaoke/Spotify_KaraokeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spotify_KaraokeApp.swift
+//  Spotify-Karaoke
+//
+//  Created by takumi saito on 2021/01/23.
+//
+
+import SwiftUI
+
+@main
+struct Spotify_KaraokeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
